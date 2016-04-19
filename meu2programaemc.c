30a7a3e670dedf7fd7f7126cd3e6b72803d6889d
Rafael Fonseca da Silva <rafafonks@gmail.com>
2016-04-19 01:05:37 +0000

    primeira aula de c

---
+++ meu2programaemc.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf ("\nBoa Noite!\n");
+    printf ("\nMeu Nome eh Rafael Fonseca\n");
+    printf ("\nEste eh meu segundo programa em C\n");
+    return 0; 
+}
